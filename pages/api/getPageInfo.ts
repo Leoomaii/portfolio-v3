12ae8aa348276ca43a2f0327d9a92effda811f10
Leonardo <leonardo-mai@outlook.com>
2022-10-12 00:41:28 +1100

    bug

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -9,6 +9,7 @@ const query = groq`
 
 type Data = {
     pageInfo: PageInfo;
+    
 }
 
 
@@ -17,7 +18,6 @@ export default async function handler(
     res: NextApiResponse<Data>
   ) {
     const pageInfo: PageInfo = await sanityClient.fetch(query)
-    
+
     res.status(200).json({ pageInfo })
-  }
-  
+  }  
